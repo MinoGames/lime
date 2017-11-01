b034e3fb703fa71c881b4fe84ebc5acb294de6cd
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-11-01 13:08:19 -0400

    Revert "[ADD] Created a callback method for grant/deny permissions."
    This reverts commit 928ffad36f73426eef16e4fd0759be55017eae3a.

--- templates/android/template/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ templates/android/template/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -29,7 +29,6 @@ import android.graphics.drawable.Drawable;
 import android.media.*;
 import android.hardware.*;
 import android.content.pm.ActivityInfo;
-import com.anthonycr.grant.PermissionsManager;
 
 /**
     SDL Activity
@@ -199,14 +198,6 @@ public class SDLActivity extends Activity {
                 SDLActivity.onNativeDropFile(filename);
             }
         }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, 
-				    String[] permissions, 
-				    int[] grantResults) {
-       Log.d(TAG, "onRequestPermissionsResult()");
-       PermissionsManager.getInstance().notifyPermissionsChange(permissions, grantResults);
     }
 
     // Events
