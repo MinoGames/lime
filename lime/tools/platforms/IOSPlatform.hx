b63d9feccdf6fada882369112546cd09ec9e0e64
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2019-02-13 00:00:38 -0500

    Remove traces

--- lime/tools/platforms/IOSPlatform.hx
+++ lime/tools/platforms/IOSPlatform.hx
@@ -141,10 +141,7 @@ class IOSPlatform extends PlatformTarget {
 		var context = project.templateContext;
 
 		// Add haxedef to Template
-		trace('Setting haxedefs');
-		context.JD = true;
 		for (def in project.haxedefs.keys()) {
-			trace('haxedef', def, project.haxedefs.get(def));
 			Reflect.setField(context, def, project.haxedefs.get(def));	
 		}
 		
