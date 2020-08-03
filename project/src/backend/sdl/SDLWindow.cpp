bbd5a1dd88c7c3174da6fc0447f9af1adb2dc8c1
Minh Ha <minh.ha@minogames.com>
2020-08-03 13:33:52 -0700

    Revert "Copy haxelime/lime commit 3000f74 Revert unneeded window changes for Android fullscreen"
    This reverts commit 1c804360941ae48fc452287eceab1c77d44752a3.

--- project/src/backend/sdl/SDLWindow.cpp
+++ project/src/backend/sdl/SDLWindow.cpp
@@ -45,7 +45,11 @@ namespace lime {
 
 		int sdlWindowFlags = 0;
 
+		#ifdef ANDROID
+		if (flags & WINDOW_FLAG_FULLSCREEN) sdlWindowFlags |= SDL_WINDOW_FULLSCREEN;
+		#else
 		if (flags & WINDOW_FLAG_FULLSCREEN) sdlWindowFlags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
+		#endif
 		if (flags & WINDOW_FLAG_RESIZABLE) sdlWindowFlags |= SDL_WINDOW_RESIZABLE;
 		if (flags & WINDOW_FLAG_BORDERLESS) sdlWindowFlags |= SDL_WINDOW_BORDERLESS;
 		if (flags & WINDOW_FLAG_HIDDEN) sdlWindowFlags |= SDL_WINDOW_HIDDEN;
@@ -290,6 +294,13 @@ namespace lime {
 
 		}
 
+		#ifdef ANDROID
+		// TODO: Is this extra call needed?
+		if (flags & WINDOW_FLAG_FULLSCREEN)
+		{
+			SetFullscreen(true);
+		}
+		#endif
 	}
 
 
@@ -925,7 +936,9 @@ namespace lime {
 	bool SDLWindow::SetFullscreen (bool fullscreen) {
 
 		if (fullscreen) {
-
+			#ifdef ANDROID
+			SDL_SetWindowFullscreen (sdlWindow, SDL_WINDOW_FULLSCREEN);
+			#else
 			if (displayModeSet) {
 
 				SDL_SetWindowFullscreen (sdlWindow, SDL_WINDOW_FULLSCREEN);
@@ -935,6 +948,7 @@ namespace lime {
 				SDL_SetWindowFullscreen (sdlWindow, SDL_WINDOW_FULLSCREEN_DESKTOP);
 
 			}
+			#endif
 
 		} else {
 
