bf8c4a02bca037342f23114687f91cbe65bc049c
Minh Ha <minh.ha@minogames.com>
2021-08-18 15:41:47 -0700

    Fix compilation error

--- src/lime/system/Clipboard.hx
+++ src/lime/system/Clipboard.hx
@@ -8,7 +8,7 @@ import flash.desktop.Clipboard as FlashClipboard;
 #elseif (js && html5)
 import lime._internal.backend.html5.HTML5Window;
 #end
-
+typedef VoidFunc = Void -> Void;
 #if !lime_debug
 @:fileXml('tags="haxe,release"')
 @:noDebug
@@ -17,17 +17,22 @@ import lime._internal.backend.html5.HTML5Window;
 @:access(lime.ui.Window)
 class Clipboard
 {
-	public static var onUpdate = new Event<Void->Void>();
+	public static var onUpdate = new Event<VoidFunc>();
 	public static var text(get, set):String;
 	private static var _text:String;
 
 	private static function __update():Void
 	{
 		var cacheText = _text;
+		_text = null;
 
 		#if (lime_cffi && !macro)
 		#if hl
-		_text = @:privateAccess String.fromUTF8(NativeCFFI.lime_clipboard_get_text());
+		var utf = NativeCFFI.lime_clipboard_get_text();
+		if (utf != null)
+		{
+			_text = @:privateAccess String.fromUTF8(utf);
+		}
 		#else
 		_text = NativeCFFI.lime_clipboard_get_text();
 		#end
@@ -36,7 +41,6 @@ class Clipboard
 		{
 			_text = FlashClipboard.generalClipboard.getData(TEXT_FORMAT);
 		}
-		_text = null;
 		#end
 
 		if (_text != cacheText)
