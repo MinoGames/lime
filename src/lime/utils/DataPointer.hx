5c86152cff4d1e85b37cc5c6f502b1dc34807794
Minh Ha <minh.ha@minogames.com>
2021-08-24 11:15:39 -0700

    Fix HTML5 and Android build

--- src/lime/utils/DataPointer.hx
+++ src/lime/utils/DataPointer.hx
@@ -70,8 +70,10 @@ abstract DataPointer(DataPointerType) to DataPointerType
 		if (pointer == null || pointer.bytes == null) return cast 0;
 		var data:Float = NativeCFFI.lime_bytes_get_data_pointer_offset(pointer.bytes, pointer.offset);
 		return new DataPointer(data);
+		#elseif (js && !doc_gen)
+		return new DataPointer(new DataPointerObject(null, pointer.bytes.getData(), pointer.offset));
 		#else
-		return 0;
+		return null;
 		#end
 	}
 
@@ -84,8 +86,10 @@ abstract DataPointer(DataPointerType) to DataPointerType
 		if (arrayBufferView == null) return cast 0;
 		var data:Float = NativeCFFI.lime_bytes_get_data_pointer_offset(arrayBufferView.buffer, arrayBufferView.byteOffset);
 		return new DataPointer(data);
+		#elseif (js && !doc_gen)
+		return new DataPointer(new DataPointerObject(arrayBufferView));
 		#else
-		return 0;
+		return null;
 		#end
 	}
 
@@ -94,8 +98,10 @@ abstract DataPointer(DataPointerType) to DataPointerType
 		#if (lime_cffi && !macro)
 		if (buffer == null) return cast 0;
 		return fromBytes(buffer);
+		#elseif (js && !doc_gen)
+		return new DataPointer(new DataPointerObject(buffer));
 		#else
-		return 0;
+		return null;
 		#end
 	}
 
@@ -108,8 +114,10 @@ abstract DataPointer(DataPointerType) to DataPointerType
 		if (bytes == null) return cast 0;
 		var data:Float = NativeCFFI.lime_bytes_get_data_pointer(bytes);
 		return new DataPointer(data);
+		#elseif (js && !doc_gen)
+		return fromArrayBuffer(bytes.getData());
 		#else
-		return 0;
+		return null;
 		#end
 	}
 
@@ -118,8 +126,10 @@ abstract DataPointer(DataPointerType) to DataPointerType
 		#if (lime_cffi && !macro)
 		if (bytesData == null) return cast 0;
 		return fromBytes(Bytes.ofData(bytesData));
+		#elseif (js && !doc_gen)
+		return fromArrayBuffer(bytesData);
 		#else
-		return 0;
+		return null;
 		#end
 	}
 
@@ -135,7 +145,7 @@ abstract DataPointer(DataPointerType) to DataPointerType
 		if (pointer == null) return cast 0;
 		return new DataPointer(pointer.get());
 		#else
-		return 0;
+		return null;
 		#end
 	}
 	#end
@@ -145,9 +155,208 @@ abstract DataPointer(DataPointerType) to DataPointerType
 		#if (lime_cffi && !macro)
 		return fromBytes(LimeBytes.fromFile(path));
 		#else
-		return 0;
+		return null;
 		#end
 	}
+
+	#if (js && html5 && !doc_gen)
+	@:dox(hide) @:noCompletion public function toBufferOrBufferView(?length:Int):Dynamic
+	{
+		var data:DataPointerObject = this;
+		untyped __js__("if (!data) return null");
+
+		switch (data.type)
+		{
+			case BUFFER_VIEW:
+				if (length == null) length = data.bufferView.byteLength;
+
+				if (data.offset == 0 && length == data.bufferView.byteLength)
+				{
+					return data.bufferView;
+				}
+				else
+				{
+					return new UInt8Array(data.bufferView.buffer, data.bufferView.byteOffset + data.offset, length);
+				}
+
+			case BUFFER:
+				if (length == null) length = data.buffer.byteLength;
+
+				if (data.offset == 0 && length == data.buffer.byteLength)
+				{
+					return data.buffer;
+				}
+				else
+				{
+					return new UInt8Array(data.buffer, data.offset, length);
+				}
+
+			default:
+				return null;
+		}
+	}
+
+	@:dox(hide) @:noCompletion public function toBufferView(?length:Int):Dynamic
+	{
+		var data:DataPointerObject = this;
+		untyped __js__("if (!data) return null");
+
+		switch (data.type)
+		{
+			case BUFFER_VIEW:
+				if (length == null) length = data.bufferView.byteLength;
+
+				if (data.offset == 0 && length == data.bufferView.byteLength)
+				{
+					return data.bufferView;
+				}
+				else
+				{
+					return new UInt8Array(data.bufferView.buffer, data.bufferView.byteOffset + data.offset, length);
+				}
+
+			case BUFFER:
+				if (length == null) length = data.buffer.byteLength;
+				return new UInt8Array(data.buffer, data.offset, length);
+
+			default:
+				return null;
+		}
+	}
+
+	@:dox(hide) @:noCompletion public function toFloat32Array(?length:Int):Float32Array
+	{
+		var data:DataPointerObject = this;
+		untyped __js__("if (!data) return null");
+
+		switch (data.type)
+		{
+			case BUFFER_VIEW:
+				if (length == null) length = data.bufferView.byteLength;
+				if (data.offset == 0
+					&& length == data.bufferView.byteLength
+					&& untyped __js__("data.bufferView.constructor == Float32Array"))
+				{
+					return cast data.bufferView;
+				}
+				else
+				{
+					if (length > data.bufferView.byteLength) length = data.bufferView.byteLength;
+					return new Float32Array(data.bufferView.buffer, data.bufferView.byteOffset + data.offset, Std.int(length / Float32Array.BYTES_PER_ELEMENT));
+				}
+
+			case BUFFER:
+				if (length == null) length = data.buffer.byteLength;
+				return new Float32Array(data.buffer, data.offset, Std.int(length / Float32Array.BYTES_PER_ELEMENT));
+
+			default:
+				return null;
+		}
+	}
+
+	@:dox(hide) @:noCompletion public function toInt32Array(?length:Int):Int32Array
+	{
+		var data:DataPointerObject = this;
+		untyped __js__("if (!data) return null");
+
+		switch (data.type)
+		{
+			case BUFFER_VIEW:
+				if (length == null) length = data.bufferView.byteLength;
+				if (data.offset == 0
+					&& length == data.bufferView.byteLength
+					&& untyped __js__("data.bufferView.constructor == Int32Array"))
+				{
+					return cast data.bufferView;
+				}
+				else
+				{
+					return new Int32Array(data.bufferView.buffer, data.bufferView.byteOffset + data.offset, Std.int(length / Int32Array.BYTES_PER_ELEMENT));
+				}
+
+			case BUFFER:
+				if (length == null) length = data.buffer.byteLength;
+				return new Int32Array(data.buffer, data.offset, Std.int(length / Int32Array.BYTES_PER_ELEMENT));
+
+			default:
+				return null;
+		}
+	}
+
+	@:dox(hide) @:noCompletion public function toUInt8Array(?length:Int):UInt8Array
+	{
+		var data:DataPointerObject = this;
+		untyped __js__("if (!data) return null");
+
+		switch (data.type)
+		{
+			case BUFFER_VIEW:
+				if (length == null) length = data.bufferView.byteLength;
+				if (data.offset == 0
+					&& length == data.bufferView.byteLength
+					&& untyped __js__("data.bufferView.constructor == Uint8Array"))
+				{
+					return cast data.bufferView;
+				}
+				else
+				{
+					return new UInt8Array(data.bufferView.buffer, data.bufferView.byteOffset + data.offset, length);
+				}
+
+			case BUFFER:
+				if (length == null) length = data.buffer.byteLength;
+				return new UInt8Array(data.buffer, data.offset, length);
+
+			default:
+				return null;
+		}
+	}
+
+	@:dox(hide) @:noCompletion public function toUInt32Array(?length:Int):UInt32Array
+	{
+		var data:DataPointerObject = this;
+		untyped __js__("if (!data) return null");
+
+		switch (data.type)
+		{
+			case BUFFER_VIEW:
+				if (length == null) length = data.bufferView.byteLength;
+				if (data.offset == 0
+					&& length == data.bufferView.byteLength
+					&& untyped __js__("data.bufferView.constructor == Uint32Array"))
+				{
+					return cast data.bufferView;
+				}
+				else
+				{
+					return new UInt32Array(data.bufferView.buffer, data.bufferView.byteOffset + data.offset, Std.int(length / UInt32Array.BYTES_PER_ELEMENT));
+				}
+
+			case BUFFER:
+				if (length == null) length = data.buffer.byteLength;
+				return new UInt32Array(data.buffer, data.offset, Std.int(length / UInt32Array.BYTES_PER_ELEMENT));
+
+			default:
+				return null;
+		}
+	}
+
+	@:dox(hide) @:noCompletion public function toValue():Int
+	{
+		var data:DataPointerObject = this;
+		untyped __js__("if (!data) return 0");
+		untyped __js__("if (typeof data === 'number') return data");
+
+		switch (data.type)
+		{
+			case VALUE:
+				return data.offset;
+
+			default:
+				return 0;
+		}
+	}
+	#end
 
 	private static function __withOffset(data:DataPointer, offset:Int):DataPointer
 	{
@@ -156,7 +365,7 @@ abstract DataPointer(DataPointerType) to DataPointerType
 		var data:Float = NativeCFFI.lime_data_pointer_offset(data, offset);
 		return new DataPointer(data);
 		#else
-		return 0;
+		return null;
 		#end
 	}
 
@@ -252,5 +461,40 @@ abstract DataPointer(DataPointerType) to DataPointerType
 #if (lime_cffi && !js)
 private typedef DataPointerType = Float;
 #else
-private typedef DataPointerType = Int;
+private typedef DataPointerType = Dynamic;
+
+@:dox(hide) class DataPointerObject
+{
+	public var buffer:ArrayBuffer;
+	public var bufferView:ArrayBufferView;
+	public var offset:Int;
+	public var type:DataPointerObjectType;
+
+	public function new(?bufferView:ArrayBufferView, ?buffer:ArrayBuffer, offset:Int = 0)
+	{
+		if (bufferView != null)
+		{
+			this.bufferView = bufferView;
+			type = BUFFER_VIEW;
+		}
+		else if (buffer != null)
+		{
+			this.buffer = buffer;
+			type = BUFFER;
+		}
+		else
+		{
+			type = VALUE;
+		}
+
+		this.offset = offset;
+	}
+}
+
+@:dox(hide) enum DataPointerObjectType
+{
+	BUFFER;
+	BUFFER_VIEW;
+	VALUE;
+}
 #end
