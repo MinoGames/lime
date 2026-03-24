3316c49e00f248fc4af29a28bba37b8494b6e733
jadmalek <jadmalek9@hotmail.com>
2026-03-24 17:51:33 -0400

    Put Additional Fix

--- templates/android/template/app/src/main/java/org/libsdl/app/HIDDeviceManager.java
+++ templates/android/template/app/src/main/java/org/libsdl/app/HIDDeviceManager.java
@@ -568,12 +568,22 @@ public class HIDDeviceManager {
             HIDDeviceOpenPending(deviceID);
             try {
                 PendingIntent permissionIntent;
+                Intent intent = new Intent(HIDDeviceManager.ACTION_USB_PERMISSION);
+
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                    // For Android 12 (API 31) and above, use FLAG_MUTABLE or FLAG_IMMUTABLE
-                    // FLAG_MUTABLE is often necessary for broadcast intents that need to be updated
-                    permissionIntent = PendingIntent.getBroadcast(mContext, 0, new Intent(HIDDeviceManager.ACTION_USB_PERMISSION), PendingIntent.FLAG_MUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
+                    permissionIntent = PendingIntent.getBroadcast(
+                        mContext,
+                        0,
+                        intent,
+                        PendingIntent.FLAG_MUTABLE | PendingIntent.FLAG_UPDATE_CURRENT
+                    );
                 } else {
-                    permissionIntent = PendingIntent.getBroadcast(mContext, 0, new Intent(HIDDeviceManager.ACTION_USB_PERMISSION), 0);
+                    permissionIntent = PendingIntent.getBroadcast(
+                        mContext,
+                        0,
+                        intent,
+                        PendingIntent.FLAG_IMMUTABLE
+                    );
                 }
                 mUsbManager.requestPermission(usbDevice, permissionIntent);
             } catch (Exception e) {
